c61437dd300a7a7b921bba2ada8db3893d51510a
WYeast <56200018+WYeast@users.noreply.github.com>
2019-10-15 04:03:52 +0800

    Update calculate_midpoint.pl

--- calculate_midpoint.pl
+++ calculate_midpoint.pl
@@ -7,8 +7,8 @@ open IN,"$ARGV[0]" or die $!;    ## files to be processd
 open OUT,">$ARGV[1]" or die $!;   ## processed file
 
 while(<IN>){
-	chomp;
-	my @sp=split /\s+/;
-	my $midpoint=int(0.5*($sp[1]+$sp[2]));
-	print OUT "$sp[0]\t$midpoint\n";
+    chomp;
+    my @sp=split /\s+/;
+    my $midpoint=int(0.5*($sp[1]+$sp[2]));
+    print OUT "$sp[0]\t$midpoint\n";
 }
