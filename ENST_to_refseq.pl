959477eeb76a6da7a161b00cdaf6fade9b3efffe
WYeast <56200018+WYeast@users.noreply.github.com>
2019-10-15 04:02:40 +0800

    Update ENST_to_refseq.pl

--- ENST_to_refseq.pl
+++ ENST_to_refseq.pl
@@ -9,16 +9,16 @@ open OUT,">$ARGV[2]" or die $!;   ## processed file
 
 my %hash;
 while(<IN>){
-	chomp;
-	my @sp=split /\s+/;
-	$hash{$sp[0]}=$sp[1];
+    chomp;
+    my @sp=split /\s+/;
+    $hash{$sp[0]}=$sp[1];
 }                                       
 ## key=ENST_number. value= NM_number
 while(<IN2>){
-	chomp;
-	my @sp=split /\s+/;
-	if($hash{$sp[6]}){
-	    print OUT "$hash{$sp[6]}\t$sp[3]\n";
+    chomp;
+    my @sp=split /\s+/;
+    if($hash{$sp[6]}){
+    print OUT "$hash{$sp[6]}\t$sp[3]\n";
     }
 }
 
